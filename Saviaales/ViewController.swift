6083ab13457dfdb84990ad4aacfc9139c3f0dbf9
Amir Nuriev <nuriev.amirr@yandex.ru>
2020-10-23 13:45:51 +0200

    Initial Commit

---
+++ Saviaales/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Saviaales
+//
+//  Created by Amir Nuriev on 23.10.2020.
+//  Copyright © 2020 Amir Nuriev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
