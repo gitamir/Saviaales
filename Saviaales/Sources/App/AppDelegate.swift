7d596330983dfa6d69d1e190b7a46bad4b5918a5
Amir Nuriev <nuriev.amirr@yandex.ru>
2020-10-23 14:40:44 +0200

    Setup initial project structure

---
+++ Saviaales/Sources/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Saviaales
+//
+//  Created by Amir Nuriev on 23.10.2020.
+//  Copyright © 2020 Amir Nuriev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
